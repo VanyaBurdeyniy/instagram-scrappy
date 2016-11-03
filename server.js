05ed0ac8da73733b92d65876669d1be60caaa126
getupandgo <vanyaburd@gmail.com>
2016-11-03 03:40:43 +0200

    new functionality

--- server.js
+++ server.js
@@ -82,7 +82,8 @@ app.post('/followers', function(req, res) {
         if (err) res.status(500).jsonp(err);
         fs.writeFile(__dirname + '/public/' + req.query.name + '.csv', csv, function(data) {
             console.log(data);
-            res.status(200).jsonp({followers: followersFiltered, csv: csv, url: 'http://185.65.246.183:6230/' + req.query.name + '.csv'});
+            // res.status(200).jsonp({followers: followersFiltered, csv: csv, url: 'http://185.65.246.183:6230/' + req.query.name + '.csv'});
+            res.status(200).jsonp({followers: followersFiltered, csv: csv, url: 'http://localhost:6230/' + req.query.name + '.csv'});
         });
     });
 });
