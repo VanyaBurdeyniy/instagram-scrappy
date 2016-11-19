ab8a6c5705dd25e5b69e256b39df93366ac26f8e
getupandgo <vanyaburd@gmail.com>
2016-11-19 22:37:43 +0200

    revert to work version

--- server.js
+++ server.js
@@ -8,20 +8,11 @@ var request = require('request');
 var fs = require('fs');
 var converter = require('json-2-csv');
 var mongoose = require('mongoose');
-var cors = require('cors');
 var db = mongoose.connect('mongodb://localhost:27017/instagram-scrapper');
 require('./models/user.model');
 var User = mongoose.model('User');
 var followersFiltered;
 
-server = require('https').createServer({
-    ca: [fs.readFileSync(__dirname + '/cert/root.crt'), fs.readFileSync(__dirname + '/cert/1_cross_Intermediate.crt'), fs.readFileSync(__dirname + '/cert/2_issuer_Intermediate.crt')],
-    key: fs.readFileSync(__dirname + '/cert/4_user_doit-cheers.in.ua.key'),
-    cert: fs.readFileSync(__dirname + '/cert/3_user_doit-cheers.in.ua.crt'),
-    keyphrase: 'doit'
-}, app).listen(4433, '159.203.69.143');
-
-app.use(cors());
 app.use(express.static(__dirname + '/public'));
 
 app.use(bodyParser.json({limit: '50mb'}));
@@ -33,39 +24,39 @@ app.use(bodyParser.json());
 //if (!process.argv[2]) throw 'You must specify a web page!';
 
 var c = new Crawler({
-  maxConnections : 10,
-  callback : function (error, result, $) {
-    $('a').each( function(index, a) {
-      var toQueueUrl = $(a).attr('href');
-      c.queue(toQueueUrl);
-    });
-  }
+    maxConnections : 10,
+    callback : function (error, result, $) {
+        $('a').each( function(index, a) {
+            var toQueueUrl = $(a).attr('href');
+            c.queue(toQueueUrl);
+        });
+    }
 });
 
 //var host = 'https://api.instagram.com/query/?q=ig_user(559802352)+%7B%0A++followed_by.first(100)+%7B%0A++++count%2C%0A++++page_info+%7B%0A++++++end_cursor%2C%0A++++++has_next_page%0A++++%7D%2C%0A++++nodes+%7B%0A++++++id%2C%0A++++++is_verified%2C%0A++++++followed_by_viewer%2C%0A++++++requested_by_viewer%2C%0A++++++full_name%2C%0A++++++profile_pic_url%2C%0A++++++username%0A++++%7D%0A++%7D%0A%7D%0A&ref=relationships%3A%3Afollow_list'
 
 app.get('/userId', function(req, res) {
-  c.queue([{
-    uri: 'https://smashballoon.com/instagram-feed/find-instagram-user-id/?username=' + req.query.name,
-    jQuery: false,
-    callback: function(error, result) {
-      jsdom.env(
-          result.body,
-          ["./jquery.js"],
-          function(err, window) {
-            var userId = [];
-            var id = window.$('html').find('#show_id').find('b').each(function(e, index) {
-              if (window.$(index).text().match('User ID:')) {
-                userId.push(window.$(index).text());
-              }
-            });
-            userId = userId[0].replace('User ID:', '');
-            userId = userId.replace(' ', '');
-            res.status(200).jsonp({id: userId, name: req.query.name});
-          }
-      );
-    }
-  }]);
+    c.queue([{
+        uri: 'https://smashballoon.com/instagram-feed/find-instagram-user-id/?username=' + req.query.name,
+        jQuery: false,
+        callback: function(error, result) {
+            jsdom.env(
+                result.body,
+                ["./jquery.js"],
+                function(err, window) {
+                    var userId = [];
+                    var id = window.$('html').find('#show_id').find('b').each(function(e, index) {
+                        if (window.$(index).text().match('User ID:')) {
+                            userId.push(window.$(index).text());
+                        }
+                    });
+                    userId = userId[0].replace('User ID:', '');
+                    userId = userId.replace(' ', '');
+                    res.status(200).jsonp({id: userId, name: req.query.name});
+                }
+            );
+        }
+    }]);
 });
 
 
@@ -92,7 +83,7 @@ app.post('/followers', function(req, res) {
         fs.writeFile(__dirname + '/public/' + req.query.name + '.csv', csv, function(data) {
             console.log(data);
             // res.status(200).jsonp({followers: followersFiltered, csv: csv, url: 'http://185.65.246.183:6230/' + req.query.name + '.csv'});
-            res.status(200).jsonp({followers: followersFiltered, csv: csv, url: 'https://159.203.69.143:4433/' + req.query.name + '.csv'});
+            res.status(200).jsonp({followers: followersFiltered, csv: csv, url: 'http://159.203.69.143:6230/' + req.query.name + '.csv'});
         });
     });
 });
@@ -106,24 +97,5 @@ app.post('/bio', function(req, res) {
     });
 });
 
-
-function getPage() {
-    c.queue([{
-        uri: 'https://www.instagram.com/query/?q=ig_user(559802352)+%7B%0A++follows.first(10)+%7B%0A++++count%2C%0A++++page_info+%7B%0A++++++end_cursor%2C%0A++++++has_next_page%0A++++%7D%2C%0A++++nodes+%7B%0A++++++id%2C%0A++++++is_verified%2C%0A++++++followed_by_viewer%2C%0A++++++requested_by_viewer%2C%0A++++++full_name%2C%0A++++++profile_pic_url%2C%0A++++++username%0A++++%7D%0A++%7D%0A%7D%0A&ref=relationships%3A%3Afollow_list',
-        jQuery: false,
-        callback: function(error, result) {
-            console.log(result.body);
-            jsdom.env(
-                result.body,
-                ["./jquery.js"],
-                function(err, window) {
-                    console.log('yeeeees');
-                }
-            );
-        }
-    }]);
-}
-getPage();
-
-// app.listen(6230);
+app.listen(6230);
 console.log('Server listening on port 6230');
